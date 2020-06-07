240a3838f44e5c2081b7cde07ca46c03f1b55085
Dominik Caputa (domicap777) <domicap777@student.polsl.pl>
2020-06-07 09:35:59 +0200

    1

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Kontroller odpowiedzialny za menu
+/// </summary>
+public class MenuController : MonoBehaviour
+{
+  
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   public  void EndGame()
+    {
+        Debug.Log("wyszedłeś z gry");
+        Application.Quit();
+    }
+
+}
--- Assets/scripts/ZombieHead.cs
+++ Assets/scripts/ZombieHead.cs
@@ -6,6 +6,8 @@ public class ZombieHead : MonoBehaviour
 {
     [Header ("colider type")]
     [SerializeField] private string bodyType;
+    [SerializeField] GameObject _parent;
+
     private int dmgMultiply=3;
     public void pistolHit(int demage)
     {
@@ -13,10 +15,10 @@ public class ZombieHead : MonoBehaviour
         {
             case "head":
                 Debug.Log(bodyType+ demage * dmgMultiply);
-                transform.parent.gameObject.GetComponent<ZombieController>().pistolHit(demage* dmgMultiply);
+                _parent.gameObject.GetComponent<ZombieController>().pistolHit(demage * dmgMultiply);
                 break;
             case "chest":
-                transform.parent.gameObject.GetComponent<ZombieController>().pistolHit(demage);
+                _parent.gameObject.GetComponent<ZombieController>().pistolHit(demage);
                 Debug.Log(bodyType+ demage);
                 break;
             case "leg":
